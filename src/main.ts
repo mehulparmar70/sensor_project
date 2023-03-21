42e86b5e40f428dd6e0c3fa1370f6102288867eb
mehul <mehul@veravalonline.com>
2023-03-21 20:59:11 +0530

    Mehul

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
@@ -14,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
